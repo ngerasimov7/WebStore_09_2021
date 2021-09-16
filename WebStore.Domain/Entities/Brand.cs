004cf737ba404d13ba8f6f19c14474f70e8e7a2b
ngerasimov7 <nikola20@mail.ru>
2021-09-16 12:11:05 +0300

    Добавьте файлы проекта.

---
+++ WebStore.Domain/Entities/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    //[Table("Brands123")]
+    public class Brand : NamedEntity, IOrderedEntity //Brands
+    {
+        //[Column("BrandOrder")]
+        public int Order { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
