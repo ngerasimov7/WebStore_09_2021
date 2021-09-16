4fdd8508468317f8c2410d74a8c9868ee1114d81
ngerasimov7 <nikola20@mail.ru>
2021-09-16 16:34:59 +0300

    Интерфейс для тестового WebAPI клиента

---
+++ Services/WebStore.Interfaces/TestAPI/IValuesService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Interfaces.TestAPI
+{
+    public interface IValuesService
+    {
+        IEnumerable<string> GetAll();
+
+        string GetByIndex(int index);
+
+        void Add(string value);
+
+        void Edit(int index, string str);
+
+        bool Delete(int index);
+
+    }
+}
