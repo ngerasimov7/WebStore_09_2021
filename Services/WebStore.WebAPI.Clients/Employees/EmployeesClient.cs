84bdff4a1db1596e5a9c8ce5e19b2790c913eb34
ngerasimov7 <nikola20@mail.ru>
2021-09-21 08:35:28 +0300

    Реализация клиента для сервиса управления сотрудниками

---
+++ Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Interfaces.TestAPI;
+using System.Net.Http;
+using System.Net.Http.Json;
+using WebStore.WebAPI.Clients.Base;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmloyeesData
+    {
+        public EmployeesClient(HttpClient Client) : base(Client, "api/employees") { }
+        public IEnumerable<Employee> GetAll() 
+        {
+            var result = Get<IEnumerable<Employee>>(Address);
+            return result;
+        }
+
+        public Employee Get(int id)
+        {
+            var result = Get<Employee>($"{Address}/{id}");
+            return result;
+        }
+
+        public int Add(Employee employee) 
+        {
+            var response = Post(Address, employee);
+            var id = response.Content.ReadFromJsonAsync<int>().Result;
+            return id;
+        }
+
+        public void Update(Employee employee)
+        {
+            Put(Address, employee);
+        }
+
+        public bool Delete(int id)
+        {
+            var response = Delete($"{Address}/{id}");
+            var success = response.IsSuccessStatusCode;
+            return success;
+            //return response.Content.ReadFromJsonAsync<bool>().Result;
+        }
+
+    }
+}
