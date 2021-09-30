edd75f4142fd6be00ed589cb36c5bd9ca7044e06
ngerasimov7 <nikola20@mail.ru>
2021-09-30 20:09:58 +0300

    Ведение журнала в контроллерах Account, EmployeesController

--- UI/WebStore/Controllers/AccountController.cs
+++ UI/WebStore/Controllers/AccountController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Domain.ViewModels;
-
 namespace WebStore.Controllers
 {
     [Authorize]
@@ -15,22 +14,18 @@ namespace WebStore.Controllers
         private readonly UserManager<User> _UserManager;
         private readonly SignInManager<User> _SignInManager;
         private readonly ILogger<AccountController> _Logger;
-
         public AccountController(
             UserManager<User> UserManager,
             SignInManager<User> SignInManager,
-            ILogger<AccountController> Logger)
+            ILogger<AccountController> Logger) //WebStore.Controllers.AccountController
         {
             _UserManager = UserManager;
             _SignInManager = SignInManager;
             _Logger = Logger;
         }
-
         #region Register
-
         [AllowAnonymous]
         public IActionResult Register() => View(new RegisterUserViewModel());
-
         [AllowAnonymous]
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterUserViewModel Model)
@@ -39,55 +34,55 @@ namespace WebStore.Controllers
 
             _Logger.LogInformation("Регистрация нового пользователя {0}", Model.UserName);
 
-            var user = new User
+            using (_Logger.BeginScope("Регистрация пользователя {0}", Model.UserName))
             {
-                UserName = Model.UserName
-            };
+                var user = new User
+                {
+                    UserName = Model.UserName
+                };
 
-            var register_result = await _UserManager.CreateAsync(user, Model.Password);
-            if (register_result.Succeeded)
-            {
-                _Logger.LogInformation("Пользователь {0} успешно зарегистрирован", user.UserName);
+                var register_result = await _UserManager.CreateAsync(user, Model.Password);
+                if (register_result.Succeeded)
+                {
+                    _Logger.LogInformation("Пользователь {0} успешно зарегистрирован", user.UserName);
 
-                await _UserManager.AddToRoleAsync(user, Role.Users);
+                    await _UserManager.AddToRoleAsync(user, Role.Users);
 
-                _Logger.LogInformation("Пользователю {0} назначена роль {1}", 
-                    user.UserName, Role.Users);
+                    _Logger.LogInformation("Пользователю {0} назначена роль {1}",
+                        user.UserName, Role.Users);
 
-                //await _UserManager.RemoveFromRoleAsync(user, Role.Administrators);
+                    //await _UserManager.RemoveFromRoleAsync(user, Role.Administrators);
 
-                await _SignInManager.SignInAsync(user, false);
+                    await _SignInManager.SignInAsync(user, false);
 
-                _Logger.LogInformation("Пользователь {0} автоматически вошёл в систему после регистрации", user.UserName);
+                    _Logger.LogInformation("Пользователь {0} автоматически вошёл в систему после регистрации", user.UserName);
 
-                return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in register_result.Errors)
+                    ModelState.AddModelError("", error.Description);
+
+                _Logger.LogWarning("Ошибка при регистрации пользователя {0} в систему: {1}",
+                    Model.UserName,
+                    string.Join(", ", register_result.Errors.Select(err => err.Description)));
+
+                return View(Model);
             }
-
-            foreach (var error in register_result.Errors)
-                ModelState.AddModelError("", error.Description);
-
-            _Logger.LogWarning("Ошибка при регистрации пользователя {0} в систему: {1}",
-                Model.UserName,
-                string.Join(", ", register_result.Errors.Select(err => err.Description)));
-
-            return View(Model);
-        } 
+        }
 
         #endregion
-
         [AllowAnonymous]
         public IActionResult Login(string ReturnUrl) => View(new LoginViewModel { ReturnUrl = ReturnUrl });
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
         public async Task<IActionResult> Login(LoginViewModel Model)
         {
             if (!ModelState.IsValid) return View(Model);
-
             var login_result = await _SignInManager.PasswordSignInAsync(
                 Model.UserName,
-                Model.Password, 
+                Model.Password,
                 Model.RememberMe,
 #if DEBUG
                 false
@@ -95,9 +90,12 @@ namespace WebStore.Controllers
                 true
 #endif
                 );
-            
+
             if (login_result.Succeeded)
             {
+                //_Logger.LogInformation($"Успешный вход в систему {Model.UserName}"); // так делать не надо!
+                _Logger.LogInformation("Успешный вход в систему {0}", Model.UserName);
+
                 //return Redirect(Model.ReturnUrl); // не безопасно!
                 //if (Url.IsLocalUrl(Model.ReturnUrl))
                 //    return Redirect(Model.ReturnUrl);
@@ -106,6 +104,8 @@ namespace WebStore.Controllers
                 return LocalRedirect(Model.ReturnUrl ?? "/");
             }
 
+            _Logger.LogWarning("Ошибка ввода пользователя, или пароля {0}", Model.UserName);
+
             ModelState.AddModelError("", "Ошибка в имени пользователя, либо в пароле");
 
             return View(Model);
@@ -113,11 +113,23 @@ namespace WebStore.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            var user_name = User.Identity!.Name;
+
             await _SignInManager.SignOutAsync();
+            _Logger.LogInformation("Пользователь {0} успешно вышел из системы", user_name);
+
             return RedirectToAction("Index", "Home");
         }
 
         [AllowAnonymous]
-        public IActionResult AccessDenied() => View();
+        public IActionResult AccessDenied()
+        {
+            var user_name = User.Identity!.IsAuthenticated ? User.Identity.Name : "anonymous";
+
+            _Logger.LogWarning("В доступе отказано пользователю {0} по пути {1}",
+                user_name, ControllerContext.HttpContext.Request.Path);
+
+            return View();
+        }
     }
 }
--- UI/WebStore/Controllers/EmployeesController.cs
+++ UI/WebStore/Controllers/EmployeesController.cs
@@ -1,58 +1,46 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
+﻿using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Identity;
-using WebStore.Interfaces.Services;
 using WebStore.Domain.ViewModels;
-
+using WebStore.Interfaces.Services;
 namespace WebStore.Controllers
 {
     //[Route("Staff")]
     //[Route("Employees")]
-    [Authorize]
+    //[Authorize]
     public class EmployeesController : Controller
     {
         private readonly IEmployeesData _EmployeesData;
         private readonly ILogger<EmployeesController> _Logger;
-
         public EmployeesController(IEmployeesData EmployeesData, ILogger<EmployeesController> Logger)
         {
             _EmployeesData = EmployeesData;
             _Logger = Logger;
         }
-
         //[Route("all")]
         public IActionResult Index() => View(_EmployeesData.GetAll());
-
         //[Route("info/{id}")]
         //[Route("info-id-{id}")]
         public IActionResult Details(int id)
         {
             var employee = _EmployeesData.Get(id);
-
             if (employee == null)
                 return NotFound();
-
             return View(employee);
         }
-
         [Authorize(Roles = Role.Administrators)]
         public IActionResult Create() => View("Edit", new EmployeeViewModel());
-
         [Authorize(Roles = Role.Administrators)]
         public IActionResult Edit(int? id)
         {
             if (id is null)
                 return View(new EmployeeViewModel());
-
             var employee = _EmployeesData.Get((int)id);
             if (employee is null)
                 return NotFound();
-
             var view_model = new EmployeeViewModel
             {
                 Id = employee.Id,
@@ -63,22 +51,17 @@ namespace WebStore.Controllers
             };
             return View(view_model);
         }
-
         [HttpPost]
         [Authorize(Roles = Role.Administrators)]
         public IActionResult Edit(EmployeeViewModel Model)
         {
             if (Model.LastName == "Qwe")
                 ModelState.AddModelError("LastName", "Qwe - плохое имя!");
-
             if (Model.LastName == "Asd" && Model.Name == "Qwe")
                 ModelState.AddModelError("", "Странное сочетание имени и фамилии");
-
             if (!ModelState.IsValid)
                 return View(Model);
-
             _Logger.LogInformation("Редактирование сотрудника id:{0}", Model.Id);
-
             var employee = new Employee
             {
                 Id = Model.Id,
@@ -88,25 +71,35 @@ namespace WebStore.Controllers
                 Age = Model.Age,
             };
 
-            if (employee.Id == 0)
-                _EmployeesData.Add(employee);
-            else
-                _EmployeesData.Update(employee);
+            try
+            {
+                if (employee.Id == 0)
+                {
+                    _EmployeesData.Add(employee);
+                    _Logger.LogInformation("Создание нового сотрудника id:{0} завершено", Model.Id);
+                }
+                else
+                {
+                    _EmployeesData.Update(employee);
+                    _Logger.LogInformation("Редактирование сотрудника id:{0} завершено", Model.Id);
+                }
 
-            _Logger.LogInformation("Редактирование сотрудника id:{0} завершено", Model.Id);
-
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            catch (Exception error)
+            {
+                _Logger.LogError(error, "Ошибка при редактировании сотрудника {0}", Model.Id);
+                throw;
+            }
         }
 
         [Authorize(Roles = Role.Administrators)]
         public IActionResult Delete(int id)
         {
             if (id <= 0) return BadRequest();
-
             var employee = _EmployeesData.Get(id);
             if (employee is null)
                 return NotFound();
-
             return View(new EmployeeViewModel
             {
                 Id = employee.Id,
@@ -116,15 +109,12 @@ namespace WebStore.Controllers
                 Age = employee.Age,
             });
         }
-
         [HttpPost]
         [Authorize(Roles = Role.Administrators)]
         public IActionResult DeleteConfirmed(int id)
         {
             _Logger.LogInformation("Удаление сотрудника id:{0}", id);
-
             _EmployeesData.Delete(id);
-
             _Logger.LogInformation("Удаление сотрудника id:{0} завершено", id);
             return RedirectToAction("Index");
         }
