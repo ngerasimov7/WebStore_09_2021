f54bdf66f5b7cc769a7702796a0fe437a5748a3b
ngerasimov7 <nikola20@mail.ru>
2021-10-04 20:25:14 +0300

    Модульные тесты для модели корзины и модели-представления корзины

--- Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
+++ Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using WebStore.Domain;
 using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Services;
 using Assert = Xunit.Assert;
 
@@ -12,9 +14,7 @@ namespace WebStore.Services.Tests.Services
     public class CartServiceTests
     {
         private Cart _Cart;
-
         private Mock<IProductData> _ProductDataMock;
-
         [TestInitialize]
         public void Initialize()
         {
@@ -26,7 +26,6 @@ namespace WebStore.Services.Tests.Services
                     new() { ProductId = 2, Quantity = 3 },
                 }
             };
-
             _ProductDataMock = new Mock<IProductData>();
             _ProductDataMock
                .Setup(c => c.GetProducts(It.IsAny<ProductFilter>()))
@@ -67,11 +66,54 @@ namespace WebStore.Services.Tests.Services
                     },
                 });
         }
-
         [TestMethod]
         public void Cart_Class_ItemsCount_returns_Correct_Quantity()
         {
+            var cart = _Cart;
 
+            var expected_items_count = cart.Items.Sum(i => i.Quantity);
+
+            var actual_items_count = cart.ItemsCount;
+
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_ItemsCount()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+
+            var expected_items_count = cart_view_model.Items.Sum(i => i.Quantity);
+
+            var actual_items_count = cart_view_model.ItemsCount;
+
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_TotalPrice()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+
+            var expected_total_price = cart_view_model.Items.Sum(item => item.Quantity * item.Product.Price);
+
+            var actual_total_price = cart_view_model.TotalPrice;
+
+            Assert.Equal(expected_total_price, actual_total_price);
         }
     }
 }
