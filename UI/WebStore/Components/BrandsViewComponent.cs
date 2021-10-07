4db02f05f6f067873526ac1794ec3dc8e2b0fb67
ngerasimov7 <nikola20@mail.ru>
2021-10-07 22:25:21 +0300

    Подсветка бренда

--- UI/WebStore/Components/BrandsViewComponent.cs
+++ UI/WebStore/Components/BrandsViewComponent.cs
@@ -11,7 +11,11 @@ namespace WebStore.Components
 
         public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
 
-        public IViewComponentResult Invoke(string BrandId) => View(GetBrands());
+        public IViewComponentResult Invoke(string BrandId)
+        {
+            ViewBag.BrandId = int.TryParse(BrandId, out var id) ? id : (int?)null;
+            return View(GetBrands());
+        }
 
         public IEnumerable<BrandViewModel> GetBrands() =>
             _ProductData.GetBrands()
