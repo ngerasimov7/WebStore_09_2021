368855fe9d181757f7823a4c3aae72a931ed25c4
ngerasimov7 <nikola20@mail.ru>
2021-10-07 21:38:32 +0300

    TagHelper. Логика работы активации ссылки

---
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement(Attributes = AttributeName)]
+    public class ActiveRoute : TagHelper
+    {
+        private const string AttributeName = "ws-is-active-route";
+
+        private const string IgnoreAction = "ws-ignore-action";
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+        //[HtmlAttributeName("href")]
+        //public string Href { get; set; }
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RouteValue { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var is_ignore_action = output.Attributes.RemoveAll(IgnoreAction);
+
+            if (IsActive(is_ignore_action))
+                MakeActive(output);
+
+            output.Attributes.RemoveAll(AttributeName);
+        }
+
+        private bool IsActive(bool IsIgnoreAction)
+        {
+            var route_values = ViewContext.RouteData.Values;
+
+            var route_controller = route_values["controller"]?.ToString();
+            var route_action = route_values["action"]?.ToString();
+
+            //if (!string.IsNullOrEmpty(Action) && !string.Equals(Action, current_action))
+            //    return false;
+            if (!IsIgnoreAction && Action is { Length: > 0 } action && !string.Equals(action, route_action))
+                return false;
+
+            if (Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
+                return false;
+            foreach (var (key, value) in RouteValue)
+                if (!route_values.ContainsKey(key) || route_values[key]?.ToString() != value)
+                    return false;
+            return true;
+        }
+        private static void MakeActive(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+            if (class_attribute is null)
+                output.Attributes.Add("class", "active");
+            else
+            {
+                if (class_attribute.Value.ToString()?.Contains("active") ?? false)
+                    return;
+
+                output.Attributes.SetAttribute("class", class_attribute.Value + " active");
+            }
+        }
+    }
+}
