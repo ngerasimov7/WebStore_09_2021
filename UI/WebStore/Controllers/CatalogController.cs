22aaa7dbc4cead9f3a630e8fb171bef0960def02
ngerasimov7 <nikola20@mail.ru>
2021-10-12 18:37:32 +0300

    Подготовка контроллера товаров

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,32 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using WebStore.Domain;
-using WebStore.Infrastructure.Mapping;
-using WebStore.Interfaces.Services;
 using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+using WebStore.Infrastructure.Mapping;
 
 namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
         private readonly IProductData _ProductData;
+        private readonly IConfiguration _Configuration;
 
-        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
-
-        public IActionResult Index(int? BrandId, int? SectionId)
+        public CatalogController(IProductData ProductData, IConfiguration Configuration)
         {
+            _ProductData = ProductData;
+            _Configuration = Configuration;
+        }
+
+        public IActionResult Index(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
+        {
+            var page_size = PageSize
+                ?? (int.TryParse(_Configuration["CatalogPageSize"], out var value) ? value : null);
+
             var filter = new ProductFilter
             {
                 BrandId = BrandId,
                 SectionId = SectionId,
+                Page = Page,
+                PageSize = page_size,
             };
 
             var products = _ProductData.GetProducts(filter);
-
             return View(new CatalogViewModel
             {
                 BrandId = BrandId,
@@ -34,13 +40,11 @@ namespace WebStore.Controllers
                 Products = products.OrderBy(p => p.Order).ToView()
             });
         }
-
         public IActionResult Details(int Id)
         {
             var product = _ProductData.GetProductById(Id);
             if (product is null)
                 return NotFound();
-
             //return View(new ProductViewModel
             //{
             //    Id = product.Id,
