4001aaf92b1f6e1d544c188845b0ee10fab4bfcb
ngerasimov7 <nikola20@mail.ru>
2021-10-14 23:09:08 +0300

    Доработка Таг-Хелпера переключателя страниц

--- UI/WebStore/TagHelpers/Pageing.cs
+++ UI/WebStore/TagHelpers/Pageing.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using WebStore.Domain.ViewModels;
@@ -11,45 +10,41 @@ namespace WebStore.TagHelpers
 {
     public class Pageing : TagHelper
     {
-        private readonly IUrlHelperFactory _UrlHelperFactory;
-
         [ViewContext, HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
 
         public PageViewModel PageModel { get; set; }
-
         public string PageAction { get; set; }
-
         [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
         public Dictionary<string, object> PageUrlValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
-
-        public Pageing(IUrlHelperFactory UrlHelperFactory) => _UrlHelperFactory = UrlHelperFactory;
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             var ul = new TagBuilder("ul");
             ul.AddCssClass("pagination");
 
-            var url_helper = _UrlHelperFactory.GetUrlHelper(ViewContext);
             for (var i = 1; i <= PageModel.TotalPages; i++)
-                ul.InnerHtml.AppendHtml(CreateElement(i, url_helper));
+                ul.InnerHtml.AppendHtml(CreateElement(i));
 
             output.Content.AppendHtml(ul);
         }
 
-        private TagBuilder CreateElement(int PageNumber, IUrlHelper Url)
+        private TagBuilder CreateElement(int PageNumber)
         {
             var li = new TagBuilder("li");
             var a = new TagBuilder("a");
             a.InnerHtml.AppendHtml(PageNumber.ToString());
 
+            PageUrlValues["page"] = PageNumber;
             if (PageNumber == PageModel.Page)
                 li.AddCssClass("active");
             else
             {
-                PageUrlValues["page"] = PageNumber;
-                a.Attributes["href"] = Url.Action(PageAction, PageUrlValues);
+                a.Attributes["href"] = "#"; // Url.Action(PageAction, PageUrlValues);
             }
+
+            foreach (var (key, value) in PageUrlValues.Where(v => v.Value is not null))
+                a.MergeAttribute($"data-{key}", value.ToString());
 
             li.InnerHtml.AppendHtml(a);
             return li;
