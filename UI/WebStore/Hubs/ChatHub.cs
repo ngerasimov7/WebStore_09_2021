9365fdcb6df95e50604c6417c161fddc0309feda
ngerasimov7 <nikola20@mail.ru>
2021-10-15 09:15:30 +0300

    Настройка клиентской части SignalR

--- UI/WebStore/Hubs/ChatHub.cs
+++ UI/WebStore/Hubs/ChatHub.cs
@@ -5,6 +5,6 @@ namespace WebStore.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string Message) => await Clients.All.SendAsync("MessageFromClient", Message);
+        public async Task SendMessage(string Message) => await Clients.Others.SendAsync("MessageFromClient", Message);
     }
 }
