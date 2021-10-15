90fd978c6cd81f1cf36af09aa7a7fac55a5f4558
ngerasimov7 <nikola20@mail.ru>
2021-10-15 08:34:06 +0300

    Подготовка контроллера к тестированию AJAX

---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        private readonly Logger<AjaxTestController> _Logger;
+
+        public AjaxTestController(Logger<AjaxTestController> Logger) => _Logger = Logger;
+
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> GetJSON(int? id, string msg, int Delay = 2000)
+        {
+            _Logger.LogInformation("Получен запрос к GetJSON - id:{0}, msg:{1}, delay:{2}", id, msg, Delay);
+
+            if (Delay > 0)
+                await Task.Delay(Delay);
+
+            _Logger.LogInformation("Запрос к GetJSON - id:{0}, msg:{1}, delay:{2} обработан.", id, msg, Delay);
+
+            return Json(new
+            {
+                Message = $"Response (id:{id ?? 0}): {msg ?? "null"}",
+                ServerTime = DateTime.Now,
+            });
+        }
+
+        public async Task<IActionResult> GetHTML(int? id, string msg, int Delay = 2000)
+        {
+            _Logger.LogInformation("Получен запрос к GetHTML - id:{0}, msg:{1}, delay:{2}", id, msg, Delay);
+
+            if (Delay > 0)
+                await Task.Delay(Delay);
+
+            _Logger.LogInformation("Запрос к GetHTML - id:{0}, msg:{1}, delay:{2} обработан.", id, msg, Delay);
+
+            return PartialView("Partial/_DataView", new AjaxTestDataViewModel
+            {
+                Id = id ?? 0,
+                Message = msg,
+            });
+        }
+    }
+}
---
+++ UI/WebStore/ViewModels/AjaxTestDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebStore.ViewModels
+{
+    public class AjaxTestDataViewModel
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime ServerTime { get; set; } = DateTime.Now;
+    }
+}
